79123a39a2baabd7542b8db0f491129d12faf3bd
Mohamed Boukab <81518923+mohamedbk705@users.noreply.github.com>
2024-10-31 10:57:16 +0100

    js

--- index.js
+++ index.js
@@ -7,32 +7,7 @@ function addAime(){
     count.innerHTML=i;
 }
 
-// add comment in page details
-// function addComment() {
-//     var container = document.querySelector(".comment-box");
-//     var box_comment = document.createElement("div");
-//     box_comment.className = "comment-n";
-    
-//     var addParagraph = document.createElement("p");  
-//     addParagraph.textContent = '"hello"'; // add user comment   
-//     addParagraph.className = "comment";
-//     box_comment.appendChild(addParagraph);
-
-//     var author = document.createElement("p");
-//     author.className = "author";
-
-//     var icon = document.createElement("img");
-//     icon.src = "user.png"; 
-//     icon.className = "user-icon";
-//     author.appendChild(icon);
-
-//     var authorName = document.createTextNode(" Vous");
-//     author.appendChild(authorName);
-
-//     box_comment.appendChild(author);
-//     container.appendChild(box_comment);
-// }
-
+// add comments in page details
 function addComment() {
     var commentText = document.getElementById("userComment").value;
 
@@ -63,7 +38,7 @@ function addComment() {
 }
 
 
-// dark mode sans stockage dans le mémoire
+// dark mode without local storage
 
 function changeMode(){
     var darkMode = document.querySelector(".dark");
